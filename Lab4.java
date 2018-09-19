5476a034cab1cc6cb96d4c1d8ed4e7f35db9e786
Willem E Drescher <drescherwe@rams.adp.vcu.edu>
2018-09-19 13:33:11 -0400

    testing lab4.java

--- Lab4.java
+++ Lab4.java
@@ -1,13 +1,40 @@
 public class Lab4 { 
 
 	public static void main( String [] args )  {
-	
-	
-	}
+	try {
 
-	public static Object [] sort ( Object[] input) {
+	}
+	catch ( IllegalArgumentException e ) {
+		System.out.println(e); 
+	}
+	catch (Exception e) {
+		System.out.println(e);
+	}
+	int [] ar1 = {1,2,3,4,5};
+	int [] ar2 = {5,4,3,2,1}; 
+	int [] ar3 = {1,1,1,1,1}; 
+	int [] ar4 = {1,1,22,3,4}; 
+	int [] ar5 = null; 
+	int ar6 = 0;
+
+
+	System.out.println( sort(ar1) );  
+	System.out.println( sort(ar2) );
+	System.out.println( sort(ar3) );
+	System.out.println( sort(ar4) );
+	System.out.println( sort(ar5) );
+	System.out.println( sort(ar6) );
+	   
+	}
+        
+	public static int [] sort( int [] input ) {
+		
 	 	if ( input = null; ) {
 			throw new IllegalArgumentException(); 
+		}
+
+		if ( ! input.size < 1 ) {
+			return input;
 		}
 
 		for (int i = 0; i < input.length; i++ ) {
@@ -19,6 +46,6 @@ public class Lab4 {
 				}
 			}
 		}
-
+		return intput; 
 	} 
 }
