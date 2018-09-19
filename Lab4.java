f830dc49247050d1a0aeeebf4bd9e6c36943b9e7
Willem E Drescher <drescherwe@rams.adp.vcu.edu>
2018-09-19 13:51:16 -0400

    ran code and debugged

--- Lab4.java
+++ Lab4.java
@@ -1,51 +1,69 @@
-public class Lab4 { 
+public class Lab4 {
 
-	public static void main( String [] args )  {
-	try {
+        public static void main( String [] args )  {
 
-	}
-	catch ( IllegalArgumentException e ) {
-		System.out.println(e); 
-	}
-	catch (Exception e) {
-		System.out.println(e);
-	}
-	int [] ar1 = {1,2,3,4,5};
-	int [] ar2 = {5,4,3,2,1}; 
-	int [] ar3 = {1,1,1,1,1}; 
-	int [] ar4 = {1,1,22,3,4}; 
-	int [] ar5 = null; 
-	int ar6 = 0;
+                int [] ar1 = {1,2,3,4,5};
+                int [] ar2 = {5,4,3,2,1};
+                int [] ar3 = {1,1,1,1,1};
+                int [] ar4 = {1,1,22,3,4};
+                int [] ar5 = null;
+                int [] ar6 = {0};
 
 
-	System.out.println( sort(ar1) );  
-	System.out.println( sort(ar2) );
-	System.out.println( sort(ar3) );
-	System.out.println( sort(ar4) );
-	System.out.println( sort(ar5) );
-	System.out.println( sort(ar6) );
-	   
-	}
+                printSort(ar1);
+                printSort(ar2);
+                printSort(ar3);
+                printSort(ar4);
+
+                try {
+                        printSort(ar5);
+                }
+                catch ( IllegalArgumentException e ) {
+                        System.out.println(e);
+                }
+                catch (Exception e) {
+                        System.out.println(e);
+                }
+
+                try {
+                        printSort(ar6);
+                }
+                catch ( IllegalArgumentException e ) {
+                        System.out.println(e);
+                }
+                catch (Exception e) {
+                        System.out.println(e);
+                }
+        }
+
+        public static int [] sort( int [] input ) {
+
+                if ( input == null ) {
+                        throw new IllegalArgumentException();
+                }
+
+                if ( ! (input.length < 1) ) {
+                        return input;
+                }
+
+                for (int i = 0; i < input.length; i++ ) {
+                        for ( int k = 0; k < input.length; k++ ) {
+                                if ( input[i] > input[k] ) {
+                                        int temp = input[i];
+                                        input[i] = input[k];
+                                        input[k] = temp;
+                                }
+                        }
+                }
+                return input;
+        }
         
-	public static int [] sort( int [] input ) {
-		
-	 	if ( input = null; ) {
-			throw new IllegalArgumentException(); 
-		}
-
-		if ( ! input.size < 1 ) {
-			return input;
-		}
-
-		for (int i = 0; i < input.length; i++ ) {
-			for ( int k = 0; k < input.length; k++ ) {
-				if ( input[i] > input[k] ) {
-					temp = input[i]; 
-					input[i] = input[k];
-					input[k] = temp; 
-				}
-			}
-		}
-		return intput; 
-	} 
+        public static void printSort(int [] input) {
+        	String output = "{"; 
+        	for (int e : input) {
+        		output += e +", "; 
+        	}
+        	output = output.substring(0, output.length() - 2) + "}"; 
+        	System.out.println(output); 
+        }
 }
